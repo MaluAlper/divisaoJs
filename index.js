ba9e192c40b70c696c12946964e04c0e5f3af82f
Maria Luiza <malualper@hotmail.com>
2020-10-27 16:32:29 -0300

    parte 2

---
+++ index.js
@@ -0,0 +1,13 @@
+
+  alert("Escreva um numerador e um denominador");
+
+  let numerador= prompt("Digite o numerador");
+  
+  let denominador= prompt("Digite o denominador");
+  
+  let resultado= Number(numerador) / Number(denominador);
+  
+  if(denominador==0)
+  alert(`${numerador} não é possivel dividir por ${denominador}`);
+  else if(denominador>0)
+  alert(`${numerador} dividido por ${denominador} é = ${resultado}`);
